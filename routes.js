3279e1a706372c11f6a0f9e51f8a3f67f44de924
EDA <eda@mac-mini-1.local>
2016-12-09 09:55:57 +1300

    bird booster initial deply

---
+++ routes.js
@@ -0,0 +1,7 @@
+module.exports = {
+  getHome
+}
+
+function getHome (req,res) {
+ res.send('Our bird-booster is deployed')
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+var path = require('path')
+var express = require('express')
+var hbs = require('express-handlebars')
+var bodyParser = require('body-parser')
+var routes = require('./routes')
+
+var server = express()
+
+module.exports = server
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main.hbs'
+}))
+server.set('view engine', 'hbs')
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.static('public'))
+server.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+server.get('/', routes.getHome)
