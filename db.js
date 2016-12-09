cbacd25c916e3aa8ab97eabdbe09d7dbdbe4dc09
JessicaNeary <jsmare@gmail.com>
2016-12-09 15:40:49 +1300

    Comments saved in file and asynchronous

--- db.js
+++ db.js
@@ -4,12 +4,33 @@ var path = require('path')
 var dataPath = path.join(__dirname, 'birdData.json')
 
 module.exports = {
-  getBirdData: getBirdData
+  getBirdData,
+  readComments,
+  postComment
 }
 
 function getBirdData (callback) {
   fs.readFile(dataPath, function (err, data) {
     var birds = JSON.parse(data)
     callback(null, birds)
+  })
+}
+
+function readComments (id, callback){
+  fs.readFile("feedbackData.json", function (err, data) {
+    var comments = JSON.parse(data)[id]
+    callback(null, comments)
+  })
+}
+
+function postComment (id, comment, callback){
+  fs.readFile("feedbackData.json", function (err, data) {
+    var prevComments = JSON.parse(data)
+    if (!prevComments[id])
+      prevComments[id] = []
+    prevComments[id].push(comment)
+
+    fs.writeFile("feedbackData.json", JSON.stringify(prevComments), callback)
+
   })
 }
--- routes.js
+++ routes.js
@@ -28,24 +28,32 @@ function getBirdByID (req, res) {
   function renderBirdID (err, birds) {
     var id = req.params.id - 1
     var data = birds[id]
-    if (!birdComments[id]) {
-      birdComments[id] = []
-    }
-    data.comments = birdComments[id]
-    res.render('bird', data)
+    // if (!birdComments[id]) {
+    //   birdComments[id] = []
+    // }
+    //data.comments = birdComments[id]
+    db.readComments(req.params.id, function(err, comments){
+      data.comments = comments;
+      res.render('bird', data)
+    })
   }
 }
 
 function postBird (req, res) {
-  var id = req.params.id - 1
-  if (!birdComments[id]) {
-    birdComments[id] = []
-  }
-  birdComments[id].push({
-    name: req.body.name,
-    text: req.body.text
-  })
-  res.redirect('/bird/' + req.params.id)
+  var id = req.params.id
+  db.postComment(id, {
+     name: req.body.name,
+     text: req.body.text
+   }, function(err, data) {
+     res.redirect('/bird/' + id)
+   })
+  // if (!birdComments[id]) {
+  //   birdComments[id] = []
+  // }
+  // birdComments[id].push({
+  //   name: req.body.name,
+  //   text: req.body.text
+  // })
 }
 
 function getAbout (req,res) {
