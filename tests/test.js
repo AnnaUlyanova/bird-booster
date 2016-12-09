51d463ff41ee4b2b402069fe2f2a727f0a1f0a3a
EDA <eda@mac-mini-1.local>
2016-12-09 13:59:30 +1300

    wrote unit test

--- tests/test.js
+++ tests/test.js
@@ -3,3 +3,15 @@ var request = require ('supertest')
 var cheerio = require('cheerio')
 
 var server = require('../server')
+
+test('server is returning homepage', function(t) {
+  var expected = "Bird Booster 9000";
+  request(server)
+  .get('/')
+  .end(function (err, res) {
+    var $ = cheerio.load(res.text);
+    var actual = $('title').text();
+    t.equal(actual, expected, "found title");
+    t.end();
+  })
+})
