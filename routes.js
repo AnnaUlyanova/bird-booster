5069bc9e78a44cc8c1f18637f66f2ec5ce711684
JessicaNeary <jsmare@gmail.com>
2016-12-09 14:54:34 +1300

    Added comments

--- routes.js
+++ routes.js
@@ -3,8 +3,13 @@ var db = require('./db')
 module.exports = {
   getHome,
   getBirdByID,
-  getAbout
+  getAbout,
+  postBird
 }
+
+var birdComments = {}
+
+
 
 function getHome (req,res) {
   db.getBirdData(renderBirds)
@@ -21,9 +26,26 @@ function getBirdByID (req, res) {
   db.getBirdData(renderBirdID)
 
   function renderBirdID (err, birds) {
-    var data = birds[req.params.id-1]
-  res.render('bird', data)
+    var id = req.params.id - 1
+    var data = birds[id]
+    if (!birdComments[id]) {
+      birdComments[id] = []
+    }
+    data.comments = birdComments[id]
+    res.render('bird', data)
   }
+}
+
+function postBird (req, res) {
+  var id = req.params.id - 1
+  if (!birdComments[id]) {
+    birdComments[id] = []
+  }
+  birdComments[id].push({
+    name: req.body.name,
+    text: req.body.text
+  })
+  res.redirect('/bird/' + req.params.id)
 }
 
 function getAbout (req,res) {
